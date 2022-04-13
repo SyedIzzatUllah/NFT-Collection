ae8209ed115a78249aafdb20fbc7f6f6953351a2
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-04-13 23:25:52 +0500

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -3,4 +3,4 @@ const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62f7b5bc735a64942dbec9456954c7a368a";
 // URL for the Crypto Devs NFT Metadata
 const METADATA_URL = "https://nft-collection-swart.vercel.app/api/";
 
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }; 
